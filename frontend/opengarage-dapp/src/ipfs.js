aa64ddd8952abc18c33470207db524e4d93c25c0
acalifano <a.califano123@gmail.com>
2025-01-03 11:34:17 +0100

    ipfs url updated

--- frontend/opengarage-dapp/src/ipfs.js
+++ frontend/opengarage-dapp/src/ipfs.js
@@ -1,9 +1,9 @@
 import { create } from 'ipfs-http-client';
 
 const ipfs = create({
-    host: 'localhost',  // Cambia con l'indirizzo del tuo nodo
-    port: 5001,         // Porta dell'API HTTP
-    protocol: 'http',   // Usa 'https' se stai usando un nodo remoto
+    host: '2d68-158-47-226-4.ngrok-free.app',  // Cambia con l'indirizzo del tuo nodo
+    port: 443,         // Porta dell'API HTTP
+    protocol: 'https',   // Usa 'https' se stai usando un nodo remoto
 });
 
 export default ipfs;
